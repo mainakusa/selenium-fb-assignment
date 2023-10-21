e2a76584bcbfb474b36b408caaf184fdf1428a2b
Mainak Banerjee <mainak@Mainaks-MacBook-Pro.local>
2023-10-21 14:51:14 -0400

    Pushing selenium fb project to Local Git repository

---
+++ src/main/java/com/assignment/selenium_fb_login/App.java
@@ -0,0 +1,33 @@
+package com.assignment.selenium_fb_login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws InterruptedException
+    {
+        System.setProperty("webdriver.chrome.driver", "/Users/mainak/Documents/Devops-Bootcamp/Softwares/chromedriver-mac-x64/chromedriver");
+        
+        ChromeOptions chromeOptions = new ChromeOptions();
+        
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        
+        driver.get("https://www.facebook.com");
+        
+        driver.findElement(By.id("email")).sendKeys("mb2023@yahoo.co.in");
+        driver.findElement(By.id("pass")).sendKeys("mb2023");
+        Thread.sleep(2000);
+        
+        driver.findElement(By.name("login")).click();
+        Thread.sleep(30000);
+        
+        driver.close();
+    }
+}
